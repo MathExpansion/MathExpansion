2bf44d09431ef72a737afd41b4b1c34169d17987
Placeless <mikoprpr@hotmail.com>
2023-12-03 21:52:34 +0900

    ライセンス改行

--- src/funcs.ts
+++ src/funcs.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 // リープマンの式の整数引数に対する実装
 const riemannZeta = function riemannZeta(n: number): number {
     if (n === 1) { 
